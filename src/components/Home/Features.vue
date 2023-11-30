91c0c0629648290fd148ff4cbcfc8704e384a836
arjun.airtory <arjun12345krishna@gmail.com>
2023-12-01 02:02:20 +0530

    WIP : Home

---
+++ src/components/Home/Features.vue
@@ -0,0 +1,73 @@
+
+<template>
+
+
+    <section class="my-8 bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <div class="max-w-screen-md mb-8 lg:mb-16">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">What we do at <span class="text-primary">Inovus Labs</span>.</h2>
+                <p class="text-gray-500 sm:text-xl dark:text-gray-400">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
+            </div>
+            <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+                
+                <template v-for="feature in features">
+                    <div>
+                        <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary lg:h-12 lg:w-12 dark:bg-primary-900">
+                            <!-- <svg class="w-5 h-5 text-white lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg> -->
+                            <div v-html="feature.icon"></div>
+                        </div>
+                        <h3 class="mb-2 text-xl font-bold dark:text-white">{{ feature.title }}</h3>
+                        <p class="text-gray-500 dark:text-gray-400">{{ feature.description }}</p>
+                    </div>
+                </template>
+
+            </div>
+        </div>
+    </section>
+
+
+</template>
+
+
+<script>
+    export default {
+        name: 'Features',
+        data() {
+            return {
+                features: [
+                    {
+                        title: 'Funding Support',
+                        description: 'Every year we provides financial support to a good number of students for developing innovative products. Up to one lakh rupees is made available for each idea.',
+                        icon: '<svg class="w-5 h-5 text-white lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>'
+                    },
+                    {
+                        title: 'Internships',
+                        description: 'Certified Internships provided helps our fellows to explore and learn some career challenging skills like Node.js, Machine Learning 101, Arduino 101 and Graphics Designing 101.',
+                        icon: '<svg class="w-5 h-5 text-white lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>'
+                    },
+                    {
+                        title: 'Nano Degree Certification',
+                        description: 'We associate with Kasperob Robotics, Google, Mozilla, Amazon, Vive and GitHub to help our fellows to answer a query in most interviews; "What you have in extra".',
+                        icon: '<svg class="w-5 h-5 text-white lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>'
+
+                    },
+                    {
+                        title: 'IoT Worklab',
+                        description: 'Inovus is the hub for someone who loves to try something really crazy with circuits and makes mistakes, as we know that today\'s stupidity is tomorrow\'s wonder.',
+                        icon: '<svg class="w-5 h-5 text-white lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>'
+                    },
+                    {
+                        title: 'Technical Support',
+                        description: 'Mentoring and Infrastructural support are provided to nurish the projects to be independent Start-Ups. Also, we arrange Workshops and classes to promote technology based innovation and entrepreneurship among the students.',
+                        icon: '<svg class="w-5 h-5 text-white lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>'
+                    },
+                    {
+                        title: 'Other Activities',
+                        description: 'Android Development, Women Empowerment programs, Idea Contests, Fire-Side Chats, Funding for attending premium events, Idea brain-storming Workshops, Technical support of all events conducted in the college .',
+                        icon: '<svg class="w-5 h-5 text-white lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>'
+                    }
+                ]
+            }
+        },
+    }
+</script>
---
+++ src/components/Home/Testimonial.vue
@@ -0,0 +1,87 @@
+
+<template>
+
+
+
+
+
+    <div id="default-carousel" class="bg-bgPrimary relative w-full" data-carousel="slide">
+        <div class="relative overflow-hidden rounded-lg h-80">
+
+
+            <template
+                v-for="testimonial in testimonials"
+                :key="testimonial.id"
+                >
+                
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <section class="bg-bgPrimary pt-4">
+                        <div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+                            <figure class="max-w-screen-md mx-auto">
+                                <svg class="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+                                        fill="currentColor" />
+                                </svg>
+                                <blockquote>
+                                    <p class="text-xl font-medium text-gray-900 dark:text-white">
+                                        {{ testimonial.text }}
+                                    </p>
+                                </blockquote>
+                                <figcaption class="flex items-center justify-center mt-6 space-x-3">
+                                    <img class="w-6 h-6 rounded-full"
+                                        :src="testimonial.image"
+                                        :alt="testimonial.name">
+                                    <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                                        <div class="pr-3 font-medium text-gray-900 dark:text-white">{{ testimonial.name }}</div>
+                                        <div class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">{{ testimonial.position }}</div>
+                                    </div>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </section>
+                </div>
+                    
+            </template>
+
+
+        </div>
+    </div>
+
+
+</template>
+
+
+<script>
+export default {
+    name: 'Testimonial',
+    data() {
+        return {
+            testimonials: [
+                {
+                    id: 3,
+                    name: 'Arjun Krishna',
+                    position: 'Mentor',
+                    image: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png',
+                    text: 'We encourage our fellows to think out of the box in every sort of activities and promote stupid ideas, as, today\'s stupidity is tomorrow\'s wonder.'
+                },
+                {
+                    id: 1,
+                    name: 'Roji Thomas',
+                    position: 'Nodal Officer',
+                    image: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png',
+                    text: 'Inovus Labs is a platform for developing entrepreneurial culture in our college. We here are not teaching to see the dreams, but to fulfill them.'
+                },
+                {
+                    id: 2,
+                    name: 'Jithin Shah M A',
+                    position: 'Founder',
+                    image: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png',
+                    text: 'We at Inovus Labs not only activate people to their full potential, but also develop a personalized experience that make our collegues feel more at home.'
+                },
+            ]
+        }
+    }
+}
+</script>
