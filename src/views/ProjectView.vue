a4aafbb533c2ca9cea09de9f089f3637960dcbc2
arjun.airtory <arjun12345krishna@gmail.com>
2023-12-02 04:27:22 +0530

    WIP : Blogs

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -39,10 +39,10 @@
 
 
 
-  <div class="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
+  <!-- <div class="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
 
-      <div class="grid grid-cols-1 gap-4 sm:grid-cols-3">
+      <div class="grid grid-cols-1 gap-4 sm:grid-cols-4">
 
         <template v-for="project in projects" :key="project.id">
           <ProjectCard
@@ -53,7 +53,27 @@
       </div>
 
     </div>
-  </div>
+  </div> -->
+
+
+
+  <section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+            <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Blog</h2>
+            <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
+        </div> 
+        <div class="grid gap-8 lg:grid-cols-2">
+            
+          <template v-for="project in projects" :key="project.id">
+            <ProjectCard
+              :data="project"
+            />
+          </template>
+
+        </div>  
+    </div>
+  </section>
 
 
   <Footer />
