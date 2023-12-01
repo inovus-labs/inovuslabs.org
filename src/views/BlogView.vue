9330245b08f0ef0839f42a2efe675fd4b04c7734
arjun.airtory <arjun12345krishna@gmail.com>
2023-12-02 04:32:42 +0530

    Blog

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -15,23 +15,21 @@
             <h1 class="mb-4 text-4xl font-extrabold text-primary md:text-5xl lg:text-6xl">
                 Blog Odyssey: Navigating Ideas and Insights
             </h1>
-            <p class="mb-8 text-lg font-normal text-secondary lg:text-xl sm:px-16 lg:px-48">
-                Dive into Our Featured Blog Posts for a World of Perspectives!
+            <p class="mb-8 text-lg font-normal text-secondary lg:text-xl sm:px-16 lg:px-46">
+                Dive into a world of ideas with our curated collection of blogs - where knowledge meets creativity, and every post is a journey of discovery!
             </p>
             <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-            <a href="#"
+            <a href="https://blog.inovuslabs.org/" target="_blank"
                 class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded bg-primary">
-                Get started
-                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9" />
+                Start reading
+                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                 </svg>
             </a>
-            <a href="#"
+            <!-- <a href="#"
                 class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-secondary rounded border border-secondary">
                 Learn more
-            </a>
+            </a> -->
             </div>
         </div>
 
