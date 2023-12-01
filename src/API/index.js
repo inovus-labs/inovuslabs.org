61e49b51aa19f0f2b2246b015c1e913e5222ada5
Badhusha Shaji <Badhusha.shaji22@gmail.com>
2023-12-02 02:53:34 +0530

    added projucts which are completed in project.json

--- src/API/index.js
+++ src/API/index.js
@@ -4,20 +4,24 @@ export const getProjects = async () => {
   let data = [
     {
         id: 1,
-        title: "Noteworthy technology acquisitions 2021",
-        description: "A list of noteworthy technology acquisitions 2021. This list is updated on a regular basis.",
-        thumbnail: "https://flowbite.com/docs/images/blog/image-1.jpg",
+        title: "Atom EV",
+        description: "The electric car designed and constructed by college students is an impressive blend of innovation, sustainability, and prominent technology. Crafted with a combination of academic knowledge and hands-on expertise, this vehicle represents a remarkable achievement in the world of automotive engineering",
+        thumbnail: "/projucts/Atom.jpg",
         url: "https://flowbite.com/docs",
-        tags: ["React", "Vue", "Bootstrap 5", "HTML", "CSS", "JavaScript"],
-        date: "2021-05-01",
+        tags: ["Hardware"],
         contributors: [
             {
-                name: "John Doe",
+                name: "Induchoodan R",
                 avatar: "https://flowbite.com/docs/images/avatars/avatar-1.jpg",
                 url: "https://flowbite.com/docs"
             },
             {
-                name: "Jane Doe",
+                name: "Nikhil T Das",
+                avatar: "https://flowbite.com/docs/images/avatars/avatar-2.jpg",
+                url: "https://flowbite.com/docs"
+            },
+            {
+                name: "Anuraj T R",
                 avatar: "https://flowbite.com/docs/images/avatars/avatar-2.jpg",
                 url: "https://flowbite.com/docs"
             }
@@ -26,20 +30,19 @@ export const getProjects = async () => {
 
     {
         id: 2,
-        title: "Noteworthy technology acquisitions 2021",
-        description: "A list of noteworthy technology acquisitions 2021. This list is updated on a regular basis.",
+        title: "EDU BULD",
+        description: "The LED Bulb is a remarkable achievement born from the creative minds and hard work of a group of visionary college students. This groundbreaking LED bulb is more than just a source of light a testament to innovation, sustainability, and youthful ingenuity. The students have paved a way to see big dreams for others through this business model",
         thumbnail: "https://flowbite.com/docs/images/blog/image-1.jpg",
         url: "https://flowbite.com/docs",
-        tags: ["React", "Vue", "Bootstrap 5", "HTML", "CSS", "JavaScript"],
-        date: "2021-05-01",
+        tags: ["Hardware"],
         contributors: [
             {
-                name: "John Doe",
+                name: "Nikhil T Das",
                 avatar: "https://flowbite.com/docs/images/avatars/avatar-1.jpg",
                 url: "https://flowbite.com/docs"
             },
             {
-                name: "Jane Doe",
+                name: "Indhuchoodan R ",
                 avatar: "https://flowbite.com/docs/images/avatars/avatar-2.jpg",
                 url: "https://flowbite.com/docs"
             }
@@ -48,12 +51,11 @@ export const getProjects = async () => {
 
     {
         id: 3,
-        title: "Noteworthy technology acquisitions 2021",
-        description: "A list of noteworthy technology acquisitions 2021. This list is updated on a regular basis.",
-        thumbnail: "https://flowbite.com/docs/images/blog/image-1.jpg",
+        title: "Smart Inovus",
+        description: "Home automation refers to the use of smart technology and integrated systems to control various aspects of a home, providing homeowners with increased convenience, energy efficiency, security, and customization. We used ESP32, 16 Channel relay module and 12v DC adapter for controlling the appliances. The ESP32 is integrated with Arduino IOT cloud platform. It provides Integration with virtual assistants like Amazon Alexa, Google Assistant, or Apple Home Kit allows users to control devices using voice commands.",
+        thumbnail: "/projucts/Automation.jpeg",
         url: "https://flowbite.com/docs",
-        tags: ["React", "Vue", "Bootstrap 5", "HTML", "CSS", "JavaScript"],
-        date: "2021-05-01",
+        tags: ["IoT"],
         contributors: [
             {
                 name: "John Doe",
@@ -70,21 +72,47 @@ export const getProjects = async () => {
 
     {
         id: 4,
-        title: "Noteworthy technology acquisitions 2021",
-        description: "A list of noteworthy technology acquisitions 2021. This list is updated on a regular basis.",
-        thumbnail: "https://flowbite.com/docs/images/blog/image-1.jpg",
+        title: "MGU Result Scrapper",
+        description: " Mahatma Gandhi University, Kottayam has used the same process to release exam results for years. A project is proposed to automate the retrieval of result screenshots from the MGU portal, sending them securely via email in PDF format. Despite technological advancements, the university's insistence on the old method raises concerns. The script mimics manual steps, addressing issues like server overload during result publication.",
+        thumbnail:  "/projucts/scrapper.jpg",
         url: "https://flowbite.com/docs",
-        tags: ["React", "Vue", "Bootstrap 5", "HTML", "CSS", "JavaScript"],
-        date: "2021-05-01",
+        tags: ["Software"],
         contributors: [
             {
-                name: "John Doe",
+                name: "Arjun Krishna",
                 avatar: "https://flowbite.com/docs/images/avatars/avatar-1.jpg",
                 url: "https://flowbite.com/docs"
-            },
+            }
+        ]
+    },
+
+    {
+        id: 5,
+        title: "MGU Result Scrapper",
+        description: " The process of releasing exam results at Mahatma Gandhi University, Kottayam remains unchanged for several years. It is the project to get the result screenshots of a list of students from the MGU result portal via email in the form of secure PDFs. In this age of advanced technology, the persistence of this archaic process raises questions. The argument against this notion is further challenged by the secure email practices of banks and financial institutions. Additionally, the current method causes a massive influx of traffic to the website upon result publication, potentially overwhelming servers. The script emulates the manual steps of retrieving results, capturing screenshots for each result, and storing them in a designated folder",
+        thumbnail: "https://flowbite.com/docs/images/blog/image-1.jpg",
+        url: "https://flowbite.com/docs",
+        tags: ["Software"],
+        contributors: [
             {
-                name: "Jane Doe",
-                avatar: "https://flowbite.com/docs/images/avatars/avatar-2.jpg",
+                name: "Arjun Krishna",
+                avatar: "https://flowbite.com/docs/images/avatars/avatar-1.jpg",
+                url: "https://flowbite.com/docs"
+            }
+        ]
+    },
+
+    {
+        id: 6,
+        title: "MGU Result Scrapper",
+        description: " The process of releasing exam results at Mahatma Gandhi University, Kottayam remains unchanged for several years. It is the project to get the result screenshots of a list of students from the MGU result portal via email in the form of secure PDFs. In this age of advanced technology, the persistence of this archaic process raises questions. The argument against this notion is further challenged by the secure email practices of banks and financial institutions. Additionally, the current method causes a massive influx of traffic to the website upon result publication, potentially overwhelming servers. The script emulates the manual steps of retrieving results, capturing screenshots for each result, and storing them in a designated folder",
+        thumbnail: "https://flowbite.com/docs/images/blog/image-1.jpg",
+        url: "https://flowbite.com/docs",
+        tags: ["Software"],
+        contributors: [
+            {
+                name: "Arjun Krishna",
+                avatar: "https://flowbite.com/docs/images/avatars/avatar-1.jpg",
                 url: "https://flowbite.com/docs"
             }
         ]
