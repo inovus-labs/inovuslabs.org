0f61204bf9d6ec5dd638d49b8f39b3888b8e1027
arjun.airtory <arjun12345krishna@gmail.com>
2023-12-05 17:45:15 +0530

    Unique Document Title for webpages

--- src/router/index.js
+++ src/router/index.js
@@ -24,62 +24,83 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: 'Home'
+      }
     },
 
     // Navbar : About pages
     {
       path: '/about',
       name: 'about',
-      component: AboutView
+      component: AboutView,
+      meta: { title: 'About' }
     },
     {
       path: '/gallery',
       name: 'gallery',
-      component: GalleryView
+      component: GalleryView,
+      meta: { title: 'Gallery' }
     },
     {
       path: '/team',
       name: 'team',
-      component: TeamView
+      component: TeamView,
+      meta: { title: 'Team' }
     },
     {
       path: '/events',
       name: 'events',
-      component: ActivityView
+      component: ActivityView,
+      meta: { title: 'Events' }
     },
     {
       path: '/news',
       name: 'news',
-      component: NewsView
+      component: NewsView,
+      meta: { title: 'News' }
     },
 
     {
       path: '/projects',
       name: 'project',
-      component: ProjectView
+      component: ProjectView,
+      meta: { title: 'Projects' }
     },
     {
       path: '/reccuring-events',
       name: 'reccuring-events',
-      component: ReccuringEventsView
+      component: ReccuringEventsView,
+      meta: { title: 'Reccuring Events' }
     },
     {
       path: '/blog',
       name: 'blog',
-      component: BlogView
+      component: BlogView,
+      meta: { title: 'Blogs' }
     },
     {
       path: '/inora',
       name: 'inora',
-      component: PodcastView
+      component: PodcastView,
+      meta: { title: 'Inora' }
     },
     {
       path: '/:pathMatch(.*)*',
       name: '404',
-      component: NotFoundView
+      component: NotFoundView,
+      meta: { title: '404' }
     }
   ]
 })
+
+
+// Change page title on route change
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} - Inovus Labs IEDC` || 'Inovus Labs IEDC'
+  next()
+})
+
 
 export default router
