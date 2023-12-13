d8742098e6610c15c68e39446b9429fe159dc1e2
arjun.airtory <arjun12345krishna@gmail.com>
2023-12-13 20:52:40 +0530

    WIP : Nanodegree

--- src/views/NanodegreeView.vue
+++ src/views/NanodegreeView.vue
@@ -12,15 +12,14 @@
 
         <div class="mx-auto max-w-screen-xl text-center w-full lg:w-2/3 py-4 lg:py-20">
             <h1 class="mb-4 text-4xl font-extrabold text-primary md:text-5xl lg:text-6xl">
-                InoRa: Amplifying Ideas, Empowering Minds
+                Nano Degrees, Infinite Possibilities!
             </h1>
             <p class="mb-8 text-lg font-normal text-secondary lg:text-xl sm:px-16 lg:px-46">
-                Discover Inspiring Stories of Impact – Tune into <span class="font-bold">The Inovus Radio</span> for Conversations with Society's Change Agents.
+                Unlock Micro Learning Marvels - Explore Cutting-Edge Nano Degree Programs Tailored for Tomorrow's Innovators.
             </p>
             <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                <a href="https://open.spotify.com/show/4QXEeK8ZWxewcXuLKCRR8R" target="_blank"
-                    class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded bg-primary">
-                    Start listening now
+                <a href="#knowmore" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded bg-primary">
+                    Start learning now
                     <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                     </svg>
@@ -34,7 +33,7 @@
 
 
 
-    <section class="lg:p-10 p-4 my-10">
+    <section id="knowmore" class="lg:p-10 p-4 my-10">
 
         <div class="mx-auto max-w-screen-xl text-center w-full p-4 lg:mb-4">
             <h1 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white">
